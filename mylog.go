655ab4205d4d84bf55342e554c6f6f259a8f300c
dale0110 <dale0110@aliyun.com>
2013-09-29 11:03:43 +0800

    Update mylog.go
    调试通过返回值问题

--- mylog.go
+++ mylog.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 
-	var mylogger *Logger
+	var mylogger *log.Logger
 	logfile, err := os.OpenFile("test.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0)
 	if err != nil {
 		fmt.Printf("%s\r\n", err.Error())
 		os.Exit(-1)
 	}
 	defer logfile.Close()
-	mylogger = log.New(logfile, "\n", log.Ldate|log.Ltime|log.Lshortfile)
+	mylogger = log.New(logfile, "\n", log.Ldate|log.Lmicroseconds|log.Lshortfile)
 	mylogger.Print("hello")
 	mylogger.Print("oh....")
 	mylogger.Fatal("test")
